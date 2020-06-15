b062a71517a3db7f3424ce3998241d27aea8117f
Anthony <a.a.donovan@outlook.com>
2020-06-15 22:36:10 +0100

    added post route

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,9 @@ router.get('/', function(req, res, next) {
   res.render('landing', { title: 'Express' });
 });
 
+router.post('/', function(req, res) {
+  console.log('lead email: ', req.body.lead_email);
+  res.redirect('/');
+});
+
 module.exports = router;
